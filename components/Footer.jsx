602513e748938989de2bf94f30e593c2e02f3090
Brandon <brandon.aron38@gmail.com>
2024-10-01 10:27:43 +0300

    Implemented Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,56 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
+
+import styles from '../styles';
+import { footerVariants } from '../utils/motion';
+
 const Footer = () => (
-  <footer>
-    footer
-  </footer>
+  <motion.footer
+    variants={footerVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="footer-gradient" />
+
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">Ai nevoie de ajutor?</h4>
+        <a href="#_" class="relative inline-flex items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500">
+<span class="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+<span class="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+<span class="relative text-white">Purple pill or blue pill, Neo?</span>
+</a>
+        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618b] rounded-[32px] gap-[12px]">
+          <img src="/chat.svg" alt="chat" className="w-[24px] h-[24px] object-contain" />
+          <span className="font-normal text-[16px] text-white">Trimite un mesaj</span>
+        </button>
+      </div>
+      
+      
+      <div className="flex flex-col">
+        <div className="mb-[50px] h-[2px] bg-white opacity-10"/>
+
+        <div className="flex items-center justify-between flex-wrap gap-4">
+          <h4 className="font-bold text-[24px] text-white">iPhoneDoctor</h4>
+          <p className="font-normal text-[14px] text-white opacity-50"> All rights reserved 2024 - 2025 iPhone Doctor</p>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px]
+                object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </motion.footer>
 );
 
 export default Footer;
