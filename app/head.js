6caf3cf2d57e5e8863cdd98af3edf4b80b5cd61b
Brandon <brandon.aron38@gmail.com>
2025-02-19 12:50:37 +0200

    Added Meta description

--- app/head.js
+++ app/head.js
@@ -2,6 +2,10 @@ const Head = () => (
   <>
     <title>iPhone Doctor</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="description"
+      content="iPhoneDoctor oferă reparații rapide și profesionale pentru iPhone, iPad, MacBook și iMac în București. Servicii hardware și software cu garanție, realizate de specialiști Apple. Contactează-ne acum pentru o diagnosticare gratuită!"
+    />
     <link rel="icon" href="/favicon.ico" />
   </>
 );
